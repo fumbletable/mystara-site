6844e970a3c36a9ed22f591b8148562b0ca88249
Damien Fegan <fumbletable@gmail.com>
2025-09-25 18:57:33 +0100

    Temp: disable baseUrl to fix homepage

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://fumbletable.github.io/mystara-site",
+  //  baseUrl: "https://fumbletable.github.io/mystara-site",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
